8792abeeb985718b36b8594f714e9e7021e97139
w01f <a.v.krupnov@gmail.com>
2012-06-08 18:41:54 +0400

    File Manager: Form Progress fix

--- FileManager/ToolForms/FormProgress.cs
+++ FileManager/ToolForms/FormProgress.cs
@@ -11,12 +11,6 @@ namespace FileManager.ToolForms
             {
                 laProgress.Font = new System.Drawing.Font(laProgress.Font.FontFamily, laProgress.Font.Size - 2, laProgress.Font.Style);
             }
-            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-            SetStyle(ControlStyles.Opaque, false);
-            SetStyle(ControlStyles.DoubleBuffer, true);
-            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-            SetStyle(ControlStyles.UserPaint, true);
-            UpdateStyles();
         }
 
         private void FormProgress_Shown(object sender, System.EventArgs e)
