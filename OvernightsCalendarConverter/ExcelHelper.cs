6342e2df2351da4c5d69992914ecc09724fe383b
w01f <a.v.krupnov@gmail.com>
2012-07-03 18:15:35 +0400

    Overnights Calendar Converter added

---
+++ OvernightsCalendarConverter/ExcelHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace OvernightsCalendarConverter
+{
+    public class ExcelHelper
+    {
+        private static object _locker = new object();
+        private Excel.Application _excelObject;
+
+        public ExcelHelper()
+        {
+        }
+
+        public bool Connect()
+        {
+            bool result = false;
+            try
+            {
+                _excelObject = new Excel.Application();
+                _excelObject.Visible = false;
+                _excelObject.DisplayAlerts = false;
+                result = true;
+
+            }
+            catch
+            {
+                _excelObject = null;
+            }
+            return result;
+        }
+
+        public void Disconnect()
+        {
+            if (_excelObject != null)
+            {
+                foreach (Excel.Workbook workbook in _excelObject.Workbooks)
+                    workbook.Close();
+                uint processId = 0;
+                WinAPIHelper.GetWindowThreadProcessId(new IntPtr(_excelObject.Hwnd), out processId);
+                Process.GetProcessById((int)processId).Kill();
+            }
+            ReleaseComObject(_excelObject);
+            GC.Collect();
+        }
+
+        public DateTime GetOvernightsDate(string filePath)
+        {
+            DateTime result =DateTime.MinValue;
+            lock (_locker)
+            {
+                if (Connect())
+                {
+                    try
+                    {
+                        Excel.Workbook workBook = _excelObject.Workbooks.Open(filePath);
+                        foreach (Excel.Worksheet worksheet in workBook.Worksheets)
+                        {
+                            object value = worksheet.Range["O1"].Value;
+                            if (value != null)
+                            {
+                                Match m = Regex.Match(value.ToString(), @"\d{1,2}[-/\.]\d{1,2}[-/\.]\d{2,4}");
+                                if (m.Success)
+                                    if (!DateTime.TryParse(m.Value, out result))
+                                        result = DateTime.Now;
+                            }
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                    Disconnect();
+                }
+                return result;
+            }
+        }
+
+        public void ReleaseComObject(object o)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(o);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                o = null;
+            }
+        }
+    }
+}
---
+++ OvernightsCalendarConverter/WinAPIHelper.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OvernightsCalendarConverter
+{
+    public enum WM
+    {
+        WM_KEYUP = 0x0101,
+        WM_KEYDOWN = 0x0100,
+        WM_LBUTTONDOWN = 0x0201,
+        WM_LBUTTONUP = 0x0202
+    }
+
+    /// <summary>Enumeration of the different ways of showing a window using 
+    /// ShowWindow</summary>
+    public enum WindowShowStyle : uint
+    {
+        /// <summary>Hides the window and activates another window.</summary>
+        /// <remarks>See SW_HIDE</remarks>
+        Hide = 0,
+        /// <summary>Activates and displays a window. If the window is minimized 
+        /// or maximized, the system restores it to its original size and 
+        /// position. An application should specify this flag when displaying 
+        /// the window for the first time.</summary>
+        /// <remarks>See SW_SHOWNORMAL</remarks>
+        ShowNormal = 1,
+        /// <summary>Activates the window and displays it as a minimized window.</summary>
+        /// <remarks>See SW_SHOWMINIMIZED</remarks>
+        ShowMinimized = 2,
+        /// <summary>Activates the window and displays it as a maximized window.</summary>
+        /// <remarks>See SW_SHOWMAXIMIZED</remarks>
+        ShowMaximized = 3,
+        /// <summary>Maximizes the specified window.</summary>
+        /// <remarks>See SW_MAXIMIZE</remarks>
+        Maximize = 3,
+        /// <summary>Displays a window in its most recent size and position. 
+        /// This value is similar to "ShowNormal", except the window is not 
+        /// actived.</summary>
+        /// <remarks>See SW_SHOWNOACTIVATE</remarks>
+        ShowNormalNoActivate = 4,
+        /// <summary>Activates the window and displays it in its current size 
+        /// and position.</summary>
+        /// <remarks>See SW_SHOW</remarks>
+        Show = 5,
+        /// <summary>Minimizes the specified window and activates the next 
+        /// top-level window in the Z order.</summary>
+        /// <remarks>See SW_MINIMIZE</remarks>
+        Minimize = 6,
+          /// <summary>Displays the window as a minimized window. This value is 
+          /// similar to "ShowMinimized", except the window is not activated.</summary>
+        /// <remarks>See SW_SHOWMINNOACTIVE</remarks>
+        ShowMinNoActivate = 7,
+        /// <summary>Displays the window in its current size and position. This 
+        /// value is similar to "Show", except the window is not activated.</summary>
+        /// <remarks>See SW_SHOWNA</remarks>
+        ShowNoActivate = 8,
+        /// <summary>Activates and displays the window. If the window is 
+        /// minimized or maximized, the system restores it to its original size 
+        /// and position. An application should specify this flag when restoring 
+        /// a minimized window.</summary>
+        /// <remarks>See SW_RESTORE</remarks>
+        Restore = 9,
+        /// <summary>Sets the show state based on the SW_ value specified in the 
+        /// STARTUPINFO structure passed to the CreateProcess function by the 
+        /// program that started the application.</summary>
+        /// <remarks>See SW_SHOWDEFAULT</remarks>
+        ShowDefault = 10,
+        /// <summary>Windows 2000/XP: Minimizes a window, even if the thread 
+        /// that owns the window is hung. This flag should only be used when 
+        /// minimizing windows from a different thread.</summary>
+        /// <remarks>See SW_FORCEMINIMIZE</remarks>
+        ForceMinimized = 11
+    }
+
+
+    class WinAPIHelper
+    {
+        #region Public constants
+        public const Byte BSF_IGNORECURRENTTASK = 2; //this ignores the current app Hex 2
+        public const Byte BSF_POSTMESSAGE = 16;  //This posts the message Hex 10
+        public const Byte BSM_APPLICATIONS = 8;  //This tells the windows message to just go to applications Hex 8
+
+
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 SWP_NOZORDER = 0x0004;
+        const UInt32 SWP_NOREDRAW = 0x0008;
+        const UInt32 SWP_NOACTIVATE = 0x0010;
+        const UInt32 SWP_FRAMECHANGED = 0x0020;  /* The frame changed: send WM_NCCALCSIZE */
+        const UInt32 SWP_SHOWWINDOW = 0x0040;
+        const UInt32 SWP_HIDEWINDOW = 0x0080;
+        const UInt32 SWP_NOCOPYBITS = 0x0100;
+        const UInt32 SWP_NOOWNERZORDER = 0x0200;  /* Don't do owner Z ordering */
+        const UInt32 SWP_NOSENDCHANGING = 0x0400;  /* Don't send WM_WINDOWPOSCHANGING */
+
+        public const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+        #endregion
+
+        #region API imports
+        [DllImport("USER32.DLL", EntryPoint = "BroadcastSystemMessageA", SetLastError = true,
+             CharSet = CharSet.Unicode, ExactSpelling = true,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern int BroadcastSystemMessage(Int32 dwFlags, ref Int32 pdwRecipients, int uiMessage, int wParam, int lParam);
+
+
+        [DllImport("USER32.DLL", EntryPoint = "RegisterWindowMessageA", SetLastError = true,
+             CharSet = CharSet.Unicode, ExactSpelling = true,
+             CallingConvention = CallingConvention.StdCall)]
+        public static extern int RegisterWindowMessage(String pString);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, WindowShowStyle nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string className, string windowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X,
+            int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr wnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetActiveWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetFocus(IntPtr hWnd);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, uint wParam, uint lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool BringWindowToTop(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+
+        [DllImport("User32.dll", EntryPoint = "ShowWindowAsync")]
+        public static extern bool ShowWindowAsync(IntPtr hWnd, WindowShowStyle cmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern bool AttachThreadInput(uint idAttach, uint idAttachTo,bool fAttach);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetCurrentThreadId();
+
+        [DllImport("uxtheme", ExactSpelling = true)]
+        public extern static Int32 DrawThemeParentBackground(IntPtr hWnd, IntPtr hdc, ref System.Drawing.Rectangle pRect);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        public static extern int StrCmpLogicalW(String x, String y);
+        #endregion
+
+        public static void MakeTopMost(IntPtr handle)
+        {
+            SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+
+        public static void MakeNormal(IntPtr handle)
+        {
+            SetWindowPos(handle, HWND_NOTOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+
+        public static void MakeTop(IntPtr handle)
+        {
+            SetWindowPos(handle, HWND_TOP, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+
+        public static void MakeBottom(IntPtr handle)
+        {
+            SetWindowPos(handle, HWND_BOTTOM, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+    }
+}
