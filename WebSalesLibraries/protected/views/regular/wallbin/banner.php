06fe517a71ad251f86ca0baf09c6cb60761ef286
w01f <a.v.krupnov@gmail.com>
2013-03-06 13:20:40 +0400

    Web SalesDepot: Link Banners bug fix

--- WebSalesLibraries/protected/views/regular/wallbin/banner.php
+++ WebSalesLibraries/protected/views/regular/wallbin/banner.php
@@ -1,13 +1,13 @@
-<div class="banner-container" <?php if (isset($tooltip)): ?>rel="tooltip"
-	 title="<? echo $tooltip; ?>"<? endif;?>>
-	<? if ($banner->showText): ?>
+<? if ($banner->showText): ?>
+<div class="banner-container" <?php if (isset($tooltip)): ?>rel="tooltip" title="<? echo $tooltip; ?>"<? endif;?> style="width: auto">
 	<img class="banner-image" src="data:image/png;base64,<? echo $banner->image;?>">
 	<span class="<?echo $isLinkBanner ? 'banner-text-link' : 'banner-text';?>"
-		  style="font-family: <?php echo $banner->font->name; ?>,serif;				font-size: <?php echo $banner->font->size; ?>pt;				font-weight: <?php echo $banner->font->isBold ? ' bold' : ' normal'; ?>;				font-style: <?php echo $banner->font->isItalic ? ' italic' : ' normal'; ?>;				color: <?php echo $banner->foreColor; ?>;">
+		  style="font-family: <?php echo $banner->font->name; ?>,serif; font-size: <?php echo $banner->font->size; ?>pt; font-weight: <?php echo $banner->font->isBold ? ' bold' : ' normal'; ?>;font-style: <?php echo $banner->font->isItalic ? ' italic' : ' normal'; ?>;color: <?php echo $banner->foreColor; ?>;">
 			<?echo $banner->text;?>
 	</span>
-	<? else: ?>
-	<?php
+</div>
+<? else: ?>
+<?php
 	$bannerMarginLeft = '0px';
 	$bannerMarginRight = '0px';
 	if ($banner->imageAlignment === 'left')
@@ -26,7 +26,7 @@
 		$bannerMarginRight = '0px';
 	}
 	?>
-	<img class="banner-image" src="data:image/png;base64,<? echo $banner->image;?>"
-		 style="margin-left: <?php echo $bannerMarginLeft; ?>; margin-right: <?php echo $bannerMarginRight; ?>;">
-	<?endif; ?>
+<div class="banner-container" <?php if (isset($tooltip)): ?>rel="tooltip" title="<? echo $tooltip; ?>"<? endif;?>>
+	<img class="banner-image" src="data:image/png;base64,<? echo $banner->image;?>" style="margin-left: <?php echo $bannerMarginLeft; ?>; margin-right: <?php echo $bannerMarginRight; ?>;">
 </div>
+<?endif; ?>
