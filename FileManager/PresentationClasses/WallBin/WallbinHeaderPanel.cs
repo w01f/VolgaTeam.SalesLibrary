02650b0e14b438480b316d3dd2be2f62aba521a0
Alexey Krupnov <a.v.krupnov@gmail.com>
2015-12-24 21:36:39 +0300

    Remove legacy code from main branch

--- FileManager/PresentationClasses/WallBin/WallbinHeaderPanel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows.Forms;
-
-namespace FileManager.PresentationClasses.WallBin
-{
-	public class WallbinHeaderPanel : Panel
-	{
-	}
-}
--- SalesDepot.CoreObjects/ToolClasses/Globals.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SalesDepot.CoreObjects.ToolClasses
-{
-	public class Globals
-	{
-		public static bool ThreadActive;
-		public static bool ThreadAborted;
-	}
-}
