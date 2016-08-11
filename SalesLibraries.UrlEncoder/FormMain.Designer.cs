43df1aac507f56744fea00222afe395ad91f9e2c
Alexey Krupnov <akrupnov@yandex.ru>
2016-08-11 21:54:20 +0300

    Utility to create xml friendly urls added

---
+++ SalesLibraries.UrlEncoder/FormMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SalesLibraries.UrlEncoder
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonConver = new System.Windows.Forms.Button();
+			this.textBoxSource = new System.Windows.Forms.TextBox();
+			this.textBoxResult = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// buttonConver
+			// 
+			this.buttonConver.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.buttonConver.Location = new System.Drawing.Point(142, 295);
+			this.buttonConver.Name = "buttonConver";
+			this.buttonConver.Size = new System.Drawing.Size(126, 34);
+			this.buttonConver.TabIndex = 0;
+			this.buttonConver.Text = "Convert";
+			this.buttonConver.UseVisualStyleBackColor = true;
+			this.buttonConver.Click += new System.EventHandler(this.buttonConver_Click);
+			// 
+			// textBoxSource
+			// 
+			this.textBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxSource.Location = new System.Drawing.Point(12, 12);
+			this.textBoxSource.Multiline = true;
+			this.textBoxSource.Name = "textBoxSource";
+			this.textBoxSource.Size = new System.Drawing.Size(386, 116);
+			this.textBoxSource.TabIndex = 1;
+			// 
+			// textBoxResult
+			// 
+			this.textBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxResult.Location = new System.Drawing.Point(12, 151);
+			this.textBoxResult.Multiline = true;
+			this.textBoxResult.Name = "textBoxResult";
+			this.textBoxResult.Size = new System.Drawing.Size(386, 116);
+			this.textBoxResult.TabIndex = 2;
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(410, 341);
+			this.Controls.Add(this.textBoxResult);
+			this.Controls.Add(this.textBoxSource);
+			this.Controls.Add(this.buttonConver);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FormMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Url Encoder";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonConver;
+		private System.Windows.Forms.TextBox textBoxSource;
+		private System.Windows.Forms.TextBox textBoxResult;
+	}
+}
+
---
+++ SalesLibraries.UrlEncoder/FormMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SalesLibraries.UrlEncoder
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonConver_Click(object sender, EventArgs e)
+		{
+			var sourceUrl = textBoxSource.Text;
+			if(String.IsNullOrEmpty(sourceUrl)) return;
+			textBoxResult.Text = sourceUrl.Replace("!", "%21").Replace("#", "%23")
+			   .Replace("$", "%24").Replace("&", "%26")
+			   .Replace("'", "%27").Replace("(", "%28")
+			   .Replace(")", "%29").Replace("*", "%2A");
+		}
+	}
+}
